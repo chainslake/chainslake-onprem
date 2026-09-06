f9af7f775cd765c07b1ad7b522fd49616d2c24bd
lakechain <lakechain.nguyen@gmail.com>
2026-09-06 08:21:06 +0000

    update init

--- docker/etc/sbin/init.sh
+++ docker/etc/sbin/init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ ! -d "/hadoop_data/hdfs/hadoop/dfs/name/current" ]; then
+if [ ! -d "/hadoop_data/hdfs/hadoop" ]; then
   echo "Change permission"
   chown -R 1000:1000 /hadoop_data
   chmod -R 770 /hadoop_data
